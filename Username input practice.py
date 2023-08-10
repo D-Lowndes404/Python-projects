b9c48195a2d86e7696219539df2be32ce24a531b
D-Lowndes404 <124521679+D-Lowndes404@users.noreply.github.com>
2023-08-09 20:46:25 -0400

    Update Username input practice.py
    Tried to use (while) loops to repeat actions when the username was invalid. Now stuck on space detection and number count working properly.

--- Username input practice.py
+++ Username input practice.py
@@ -1,13 +1,27 @@
-# Practice sheet
-print ("*must contain 3 numbers, no spaces, and less than 15 character*")
+# --- Notes ---
+# Getting stuck on the Space detection section, everything else seems to run fine as is and loops work ok.
+# Number detection does not seem to always work but feels more of the fault of line 17 - 19.
+#
+
+print ("*must contain 3 numbers, no spaces, and less than 15 characters*")
 
 Username = input("Enter a username: ")
 
-if len(Username) > 15:
+if  len(Username) <= 0:
+    print("You didn't enter anything!")
+    name = input("Please type something this time: ")
+    
+while len(Username) > 15:
     print("Your username can't be more than 15 character!")
-elif not Username.find(" ") == -1:
+    name = input("Please enter a username: ")
+
+while Username.find("") >= -1:
     print ("Your username can't have spaces!")
-elif Username.isalpha() > 3:
+    name = input("Please enter a username: ")
+
+while Username.isalpha() < 2:
     print ("Your username needs to have 3 numbers!")
+    name = input("Please enter a username: ")
+
 else:
     print(f"Welcome {Username}")
